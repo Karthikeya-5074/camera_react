9979076064f5fff9b464691a6ace7140b66bd13a
shaikshabeer <shabeer.shaik@rampgroup.com>
2025-06-19 15:54:41 +0530

    Initial commit with .gitignore

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Welcome to React Native!</Text>
+      <Text style={styles.subText}>Welcome Shabeer!</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  subText: {
+    fontSize: 18,
+    color: '#666',
+    marginTop: 10,
+  },
+});
+
+export default App;
